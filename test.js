2a0ac745228805e0c837e3aa76a46738012fe9dc
Chen <chenwangkun@outlook.com>
2024-03-24 13:58:06 +0800

    Initial commit

---
+++ test.js
@@ -0,0 +1,52 @@
+import tar from 'tar'
+import fs from "fs";
+import path from "path";
+
+
+
+// 读取tar包中的指定文件
+export function readTarFile(
+    tarFilePath,
+    targetFile,
+    encoding = 'utf-8'
+) {
+    let noTargetFile = false
+    let buffer = Buffer.alloc(0)
+    tar.list( {
+        file: tarFilePath,
+        sync: true,
+        onentry(entry) {
+            const replace = entry.path.replace('./', '')
+            if (replace === targetFile) {
+                entry.on('data', (data) => {
+                    noTargetFile = false
+                    buffer = Buffer.concat([buffer, data])
+                })
+                entry.on('end', () => {
+                    console.log('========================end====================')
+                });
+            }
+        },
+    }, [targetFile])
+    if (noTargetFile) {
+        throw new Error(`File ${targetFile} not found in ${tarFilePath}`)
+    }
+    return buffer.toString(encoding);
+}
+const tarFile = path.resolve("resource/19145683-09a5-4fc1-9f70-91632152facb.tar");
+const tarFileCopy = path.resolve("resource/19145683-09a5-4fc1-9f70-91632152facb-copy.tar");
+
+fs.copyFileSync(tarFile, tarFileCopy)
+let info = readTarFile( tarFileCopy, 'info.json');
+
+console.log("file exists:" + fs.existsSync(tarFileCopy))
+
+fs.unlinkSync(tarFileCopy)
+
+console.log("file exists:" + fs.existsSync(tarFileCopy))
+
+setTimeout(() => {
+    console.log("exit")
+}, 20000)
+
+
